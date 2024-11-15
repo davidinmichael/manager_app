54622e075c8b68c935b74f4cd846ce4699117d45
davidinmichael <davidinmichael@gmail.com>
2024-11-15 11:43:48 +0100

    Custom Auth account and manager set up.

--- account/models.py
+++ account/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from core.models import BaseModel
 
 
-# class Account():
+
+class AccountManager(BaseUserManager):
+	pass
+
+class Account(AbstractUser, BaseModel):
+	pass
