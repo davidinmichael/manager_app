630bb1aa38b1bc29820bbecce25353f1db44d121
davidinmichael <davidinmichael@gmail.com>
2024-11-15 09:55:50 +0100

    Added the core app

--- manager_app/settings/base.py
+++ manager_app/settings/base.py
@@ -27,8 +27,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	
+    "rest_framework",
     "rest_framework_simplejwt",
 	"corsheaders",
+	"core",
     "account",
 ]
 
